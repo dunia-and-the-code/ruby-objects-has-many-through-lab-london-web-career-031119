92a3fa9be03ef0a810a0107855fe15c6608baa59
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-15 14:08:49 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist 
+  
+  @@all = []
+  
+  def self.all
+    @name = name
+  end
+  
+  def initialize(name, genre)
+    @name = name 
+    @genre = genre
+    @@all << self
+  end
+  
+  def new_song(name, genre)
+    
+  end
+
+  def songs 
+    songs.select { |songs| songs.}
+  end 
+
+end
